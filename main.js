7a3b045e91c62648a6bfa72674be91f71426af5f
SlavaGalko <syperhero7mbm@gmail.com>
2023-03-30 18:02:29 +0200

    version 0.0.1

---
+++ main.js
@@ -0,0 +1,49 @@
+let button_start = document.querySelector(".button_start");
+
+let minutesCounter = document.querySelector(".minutes");
+let secoundsCounter = document.querySelector(".secounds");
+
+button_start.addEventListener("click", buttonStartTimer);
+
+let minutes = 59;
+let secounds = 60;
+let timerId;
+let counter = 0;
+
+let audio = new Audio('./sound/sound.mp3');
+let audio3 = new Audio('./sound/sound3.mp3');
+
+let startStop = false;
+
+function buttonStartTimer() {
+  if (!startStop) {
+    startStop = true;
+    
+    audio3.play();
+
+    timerId = setInterval(function () {
+      secounds--;
+
+      minutesCounter.innerText = minutes;
+      secoundsCounter.innerText = secounds;
+      if (secounds == 0 && minutes !=0) {
+        secounds = 60;
+        minutes--;
+      }
+      else if (minutes == 0 && secounds == 0) {
+        audio.play();
+        clearInterval(timerId);
+        startStop = false;
+        minutes = 59;
+        secounds = 60;
+        minutesCounter.innerText = 60;
+        secoundsCounter.innerText = '00';
+      }
+    }, 1000);
+  } else {
+    audio3.play();
+    clearInterval(timerId);
+    startStop = false;
+  }
+}
+
